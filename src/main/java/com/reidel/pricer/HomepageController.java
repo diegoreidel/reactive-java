8ce155599edb6659ae71d48ce8fa5b5e85abb5f1
Diego Reidel <diego.reidel@sap.com>
2019-07-01 22:23:35 -0300

    Initial commit

---
+++ src/main/java/com/reidel/pricer/HomepageController.java
@@ -0,0 +1,14 @@
+package com.reidel.pricer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomepageController {
+
+    @GetMapping
+    public String greeting(@RequestParam(required = false, defaultValue = "") String name) {
+        return name.equals("") ? "Hey!" : "Hey " + name + "!";
+    }
+}
---
+++ src/test/java/com/reidel/pricer/PricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reidel.pricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
